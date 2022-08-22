062b06693f174a37a95299838103d429fdbb15d8
Samy <samy.belarbi@3wa.io>
2022-08-22 20:32:42 +0200

    correc

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -37,19 +37,19 @@ const Calculator = () => {
     }
   };
 
-  const importDigits = () => {
-    const digits = [];
+  // const importDigits = () => {
+  //   const digits = [];
 
-    for (let i = 1; i < 10; i++) {
-      digits.push(
-        <button key={i} onClick={() => updateCalc(i.toString())}>
-          {i}
-        </button>
-      );
-    }
+  //   for (let i = 1; i < 10; i++) {
+  //     digits.push(
+  //       <button key={i} onClick={() => updateCalc(i.toString())}>
+  //         {i}
+  //       </button>
+  //     );
+  //   }
 
-    return digits;
-  };
+  //   return digits;
+  // };
 
   const deleteLastNumber = () => {
     if (calc !== "") {
